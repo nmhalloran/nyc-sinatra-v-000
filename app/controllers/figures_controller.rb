0964ba69f53c72048b488f6926f7b6b75b40cd5c
Nicholas Halloran <nmhalloran@hotmail.com>
2017-12-20 22:23:27 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,12 +9,18 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(name: params["name"])
+    @figure = Figure.create(name: params["figure"]["name"])
+    @figure.title_ids = params["figure"]["title_ids"]
+    if !params["title"]["name"].empty? && !Title.all.detect {|title| title.name == params["title"]["name"]}
+      @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
+    end
+    @figure.landmark_ids = params["figure"]["landmark_ids"]
+    @figure.save
     redirect to "/figures/#{@figure.slug}"
   end
 
   get '/figures/:slug' do
     @figure = Figure.find_by_slug(params[:slug])
-    erb :"/figures/#{@figure.slug}"
+    erb :"/figures/show"
   end
 end
--- app/models/figure.rb
+++ app/models/figure.rb
@@ -2,4 +2,16 @@ class Figure < ActiveRecord::Base
   has_many :figure_titles
   has_many :titles, through: :figure_titles
   has_many :landmarks
+
+  def slug
+    if self.name
+      self.name.downcase.gsub(/[ .]/, "-")
+    end
+  end
+
+  def self.find_by_slug(name)
+    self.all.detect do |object|
+      object.slug == name
+    end
+  end
 end
