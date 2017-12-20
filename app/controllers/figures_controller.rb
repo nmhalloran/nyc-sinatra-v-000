7a8bf3f83825e1822a4e788fe6fdd2d86fd1f137
Nicholas Halloran <nmhalloran@hotmail.com>
2017-12-20 21:12:07 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,20 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    erb :index
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params["name"])
+    redirect to "/figures/#{@figure.slug}"
+  end
+
+  get '/figures/:slug' do
+    @figure = Figure.find_by_slug(params[:slug])
+    erb :"/figures/#{@figure.slug}"
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,29 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :"/landmarks/new"
+  end
+
+  get '/landmarks/:slug' do
+    @landmark = Landmark.find_by_slug(params[:slug])
+    erb :"/landmarks/show"
+  end
+
+  get '/landmarks/:slug/edit' do
+    @landmark = Landmark.find_by_slug(params[:slug])
+    erb :"/landmarks/edit"
+  end
+
+  post '/landmarks' do
+    redirect to "/landmarks/#{@landmark.slug}"
+  end
+
+  post '/landmarks/:slug' do
+    @landmark = Landmark.find_by_slug(params[:slug])
+    redirect to "/landmarks/#{@landmark.slug}"
+  end
 end
